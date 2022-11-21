92c387fec262ddaeb973fe13412f914778e2c7ee
purohit kumud <108115143+kumud10@users.noreply.github.com>
2022-11-21 15:41:14 +0530

    literally added the third!
    yupsy...

---
+++ js 3/1.js
@@ -0,0 +1,7 @@
+var http = require("http");
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World in Node js\n');
+
+}).listen(5000);
+console.log('Server running at http://127.0.0.1:5000/');
